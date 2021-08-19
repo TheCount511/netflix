12583336801fcc4d020a6c1a14049cf1917157d8
thecount511 <fadhil.akins@gmail.com>
2021-08-19 04:46:43 +0100

    form creation

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,11 +1,47 @@
-import {HeaderContainer} from'../containers/header'
-import {FooterContainer} from'../containers/footer'
+import React, { useState } from 'react'
+import HeaderContainer from '../containers/header';
+import FooterContainer from '../containers/footer'
+import { Form } from '../components';
 
-const Signin =()=>{
-    return(
-       <HeaderContainer>
-<p>Hello from the Sign In Page</p>
-       </HeaderContainer>  
+const Signin = () => {
+
+    const [emailAddress, setEmailAddress] = useState();
+    const [password, setPassword] = useState();
+    const [error, setError] = useState('');
+
+   const isInvalid = password === '' || emailAddress === '';
+   const handleSignIn =(event)=>{
+       event.preventDefault();
+   }
+
+    return (<>
+        <HeaderContainer>
+        </HeaderContainer>
+        <Form>
+            <Form.Title>Sign In</Form.Title>
+            {error && <Form.Error>{error}</Form.Error>}
+
+            <Form.Base onSubmit ={handleSignIn} method = "POST">
+              <Form.Input
+              type="email"
+              placeholder="Email address"
+              value={emailAddress}
+              onChange={({target})=> setEmailAddress(target.value)}/>
+
+            <Form.Input
+              type = "password"
+              placeholder="Password"
+              autoComplete ="off"
+              value={password}
+              onChange={({target})=> setPassword(target.value)}/>    
+
+              <Form.Submit disabled ={isInvalid} type="submit">
+                  Signin
+              </Form.Submit>
+            </Form.Base>
+        </Form>
+        <FooterContainer />
+    </>
     )
 }
 
